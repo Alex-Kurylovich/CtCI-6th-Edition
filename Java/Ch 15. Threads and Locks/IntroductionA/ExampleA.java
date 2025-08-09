ea80a1df2dbf3ee29e6de2231cfb112cde68d39c
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2025-08-09 14:36:47 -0400

    wait thread completion using join

--- Java/Ch 15. Threads and Locks/IntroductionA/ExampleA.java
+++ Java/Ch 15. Threads and Locks/IntroductionA/ExampleA.java
@@ -8,14 +8,22 @@ public class ExampleA {
 		thread.start();
 		
 		/* waits until earlier thread counts to 5 (slowly) */
-		while (instance.count != 5) {		
+        /*
+		while (instance.count != 5) {
 			try {
 				Thread.sleep(250);
 			} catch (InterruptedException exc) {
 				exc.printStackTrace();
 			}
 		}
-		
+        */
+        try {
+            thread.join(); // Main thread waits for workerThread to complete
+            System.out.println("Main thread resumed after worker thread completed.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restore the interrupted status
+            System.out.println("Main thread interrupted while waiting.");
+        }
 		System.out.println("Program Terminating.");
 	}
 }
--- Java/Ch 15. Threads and Locks/IntroductionB/ExampleB.java
+++ Java/Ch 15. Threads and Locks/IntroductionB/ExampleB.java
@@ -5,12 +5,22 @@ public class ExampleB {
 		ThreadExample instance = new ThreadExample();
 		instance.start();
 		
-		while (instance.count != 5) {		
+        /*
+		while (instance.count != 5) {
 			try {
 				Thread.sleep(250);
 			} catch (InterruptedException exc) {
 				exc.printStackTrace();
 			}
 		}
-	}
+        */
+        try {
+            instance.join(); // Main thread waits for workerThread to complete
+            System.out.println("Main thread resumed after worker thread completed.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restore the interrupted status
+            System.out.println("Main thread interrupted while waiting.");
+        }
+        System.out.println("Program Terminating.");
+    }
 }
